5ac41df3f0000dc9aa165035b8502ccf50b2b1c3
tech.coreydawkins <tech.coreydawkins@gmail.com>
2025-04-19 04:58:20 +0000

    CSS updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 export default function Home() {
-  return <div className="bg-white h-screen w-screen"></div>
+  return <div className="page bg-white h-screen w-screen"></div>
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <div className="">
+    <div className="container">
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <Link href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
           Big Dawks Moving Company
